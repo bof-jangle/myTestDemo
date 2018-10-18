4a680c6873cf8dbdbc089209e8240138011063f2
bof-jangle <jangle@jangle.xyz>
2018-10-18 16:52:49 +0800

    装箱 拆箱的测试

--- src/main/java/xyz/jangle/boxup/test/BoxupTest.java
+++ src/main/java/xyz/jangle/boxup/test/BoxupTest.java
@@ -1,8 +1,9 @@
 package xyz.jangle.boxup.test;
+
 /**
  * @author huhj
- * @email  jangle@jangle.xyz
- * @time   2018年9月29日 上午10:15:50
+ * @email jangle@jangle.xyz
+ * @time 2018年9月29日 上午10:15:50
  */
 public class BoxupTest {
 
@@ -20,15 +21,14 @@ public class BoxupTest {
 
 		Long g = 3L;
 
-		System.out.println("c == d ? :"+ (c == d));
-		System.out.println("cc == dd ? :"+ (cc == dd)); // false预存的自动拆箱只到达127。 2^7 -1   见Integer.IntegerCache
-		System.out.println("c == a+b ? :"+ (c == (a+b)));
-		System.out.println("cc == aa+bb ? :"+ (cc == (aa+bb)));	// true 当遇到运算符的时，会进行自动拆装。
-		System.out.println("c.equals(a+b) ? :"+ (c.equals(a+b)));
-		System.out.println("g == (a+b) ? :"+ (g == (a + b)));
-		System.out.println("g.equals(a+b) ? :" + (g.equals(a+b))); // false Long的 equeals方法做了类型的判断。
-		
-		
+		System.out.println("c == d ? :" + (c == d));
+		System.out.println("cc == dd ? :" + (cc == dd)); // false预存的自动拆箱只到达127。 2^7 -1 见Integer.IntegerCache
+		System.out.println("c == a+b ? :" + (c == (a + b)));
+		System.out.println("cc == aa+bb ? :" + (cc == (aa + bb))); // true 当遇到运算符的时，会进行自动拆装。
+		System.out.println("c.equals(a+b) ? :" + (c.equals(a + b)));
+		System.out.println("g == (a+b) ? :" + (g == (a + b)));
+		System.out.println("g.equals(a+b) ? :" + (g.equals(a + b))); // false Long的 equeals方法做了类型的判断。
+
 	}
 
 }
