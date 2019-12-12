b0755c07bb51dbbdb47e508dd380bdc59bc47031
bof-jangle <jangle@jangle.xyz>
2019-12-12 10:13:00 +0800

    关于List的测试

--- src/main/java/xyz/jangle/array/test/ArrayAsListTest.java
+++ src/main/java/xyz/jangle/array/test/ArrayAsListTest.java
@@ -1,5 +1,6 @@
 package xyz.jangle.array.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,12 +12,29 @@ import java.util.List;
 public class ArrayAsListTest {
 
 	public static void main(String[] args) {
+		int k = 0;
 		List<String> list = Arrays.asList("1","2","3");
 		for(String s : list){
 			System.out.println(s);
 		}
+		System.out.println("**********");
 //		List<String> list2 = ["1","2","3","4"];
 //		List<Integer> list3 = [1,2,3,4];		//没有这种语法糖
+		
+		List<String> list2 = new ArrayList<String>();
+		list2.add("1");
+		list2.add("2");
+		list2.add("3");
+		list2.remove(0-k);
+		k++;
+		for(String s : list2){
+			System.out.println(s);
+		}
+		System.out.println("**********");
+		list2.remove(2-k);
+		for(String s : list2){
+			System.out.println(s);
+		}
 
 	}
 
