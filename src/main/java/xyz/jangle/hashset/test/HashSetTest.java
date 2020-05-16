0316643ef8b89b9eab8fcea641782ed8661763e7
bof-jangle <jangle@jangle.xyz>
2020-05-16 19:06:06 +0800

    Set集的测试。

--- src/main/java/xyz/jangle/hashset/test/HashSetTest.java
+++ src/main/java/xyz/jangle/hashset/test/HashSetTest.java
@@ -2,6 +2,7 @@ package xyz.jangle.hashset.test;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * @author huhj
@@ -12,6 +13,50 @@ public class HashSetTest {
 
 	public static void main(String[] args) {
 
+//		testHashSetOrder();
+		testTreeSetOrder();
+	}
+	
+	/**
+	 * 树集的排序
+	 */
+	public static void testTreeSetOrder() {
+		TreeSet<String> set = new TreeSet<String>();
+		set.add("1");
+		set.add("2");
+		set.add("3");
+		set.add("asd");
+		set.add("asdf");
+		set.add("asdfg");
+		
+//		set.forEach(e -> System.out.println(e));
+		set.add("aaa");
+		set.add("bbb");
+		set.forEach(e -> System.out.println(e));
+		
+	}
+	
+	/**
+	 * 测试散列set集的排序。
+	 */
+	public static void testHashSetOrder() {
+		HashSet<String> set = new HashSet<String>();
+		set.add("1");
+		set.add("2");
+		set.add("3");
+		set.add("asd");
+		set.add("asdf");
+		set.add("asdfg");
+		
+//		set.forEach(e -> System.out.println(e));
+		set.add("aaa");
+		set.add("bbb");
+		set.forEach(e -> System.out.println(e));
+		
+		
+	}
+	
+	public static void oldMain() {
 		HashSet<String> set = new HashSet<String>();
 		boolean add = set.add("123");
 		System.out.println(add);
@@ -20,8 +65,6 @@ public class HashSetTest {
 		Object put = map.put("123", new Object());
 		System.out.println(put);
 		System.out.println(map.get("123"));
-		
-		
 	}
 
 }
