382c4635e41893662f191ce8bdcb86451a9b34fe
bof-jangle <jangle@jangle.xyz>
2020-05-16 19:07:28 +0800

    类型转换测试

---
+++ src/main/java/xyz/jangle/string/test/LongToStringTest.java
@@ -0,0 +1,13 @@
+package xyz.jangle.string.test;
+
+public class LongToStringTest {
+
+	public static void main(String[] args) {
+		long l = 12342342342L;
+		String s = String.valueOf(l);
+		String ss = l+"";
+		System.out.println(s+","+ss);
+
+	}
+
+}
